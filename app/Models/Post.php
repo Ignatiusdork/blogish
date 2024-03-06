52a8986147ce4ff99438bf40c498eda9bfdcba0f
ignatiusdork <littignatius@gmail.com>
2024-03-06 16:53:27 +0100

    routes-implementation

---
+++ app/Models/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\File;
+
+class Post {
+
+    public static function all() {
+        $files = File::files(resource_path("posts/"));
+
+        //array_map is a kind of loop that returns a new array
+        return array_map(function ($file) {
+            return $file->getContents();
+        }, $files);
+
+    }
+
+    public static function find($slug) {
+        // build up a path and check if it exist
+        if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
+            throw new ModelNotFoundException();
+        }
+
+        // cach the page into memory so we don't have to read from folder everytime
+        return cache()->remember("posts.{$slug}", 1200, function() use ($path) {
+            return file_get_contents($path);
+        });
+    }
+}
