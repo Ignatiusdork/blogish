02087d6538d88a1708c266b43b4059da9c277d80
graynce <graylawrence27@gmail.com>
2024-03-11 15:19:36 +0100

    creating-posts-models

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,28 +1,20 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-layout>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>App UI</title>
-    <link rel="stylesheet" href="/app.css"/>
-</head>
+    @foreach ($posts as $post)
 
-<body>
-    <?php foreach ($posts as $post) : ?>
         <article>
             <h1>
-                <a href="/posts/<?= $post->slug; ?>">
-                    <?= $post->title; ?>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}
                 </a>
             </h1>
 
             <div>
-                <?= $post->excerpt; ?>
+                {{ $post->excerpt }}
             </div>
 
         </article>
-    <?php endforeach; ?>
-</body>
 
-</html>
+    @endforeach
+
+</x-layout>
