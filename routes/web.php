7dfa3a619142c6db9fe5d8544e6a9ede15f38d2d
Ignatiusdork <littignatius@gmail.com>
2024-03-25 23:11:50 +0100

    mailchimp newsletter works

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
 use App\Models\Category;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\File;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,38 +28,12 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 // using wildcards to navigate to different pages eg 'posts/{post:slug}'
 
-Route::post('newsletter', function () {
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    request()->validate(['email' => 'required|email']);
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us11'
-    ]);
-
-    try {
-
-        $response = $mailchimp->lists->addListMember('c693aa1889', [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.'
-        ]);
-    }
-
-    return redirect('/')->with('success', 'You are now signed up for our newsletter!');
-
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
+
+Route::post('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
